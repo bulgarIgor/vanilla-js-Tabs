33176737d775a3ce5382f9b99beebf6f76c45168
igor <igor.bulgar@wearepropeople.com>
2018-02-21 15:18:00 +0200

    Create function for Parser Url.

--- _src_js/script.js
+++ _src_js/script.js
@@ -29,17 +29,20 @@ window.addEventListener("load", function() {
     allTabs[i].addEventListener("click", clickMyTab);
   }
 
-  var urlPars = document.createElement('a');
-  urlPars.href = "https://dev2.finastra.com/search?search=hello";
 
-  console.log(
-    urlPars.host + '\n' + // dev2.finastra.com with port "80" or "8080" or "3000"
-    urlPars.protocol + '\n' + // If we use http or https
-    urlPars.hostname + '\n' + // dev2.finastra.com only url
-    urlPars.port + '\n' + // port "80" or "8080" or "3000"
-    urlPars.pathname + '\n' + // after hostname/search, search is path name
-    urlPars.hash + '\n' + // if we use hash #my-anchor
-    urlPars.search + '\n' + // if you search something, in my case I search hello
-    urlPars.origin + '\n' // origin is full host with protocol and port, in my case https://dev2.finastra.com:port
-  );
+  function parseUrl(url){
+    var parserLink = document.createElement('a');
+    parserLink.href = url;
+    return parserLink;
+  }
+  parserLink = parseUrl("https://dev2.finastra.com/search?search=hello");
+
+  console.log(parserLink.host); // dev2.finastra.com with port "80" or "8080" or "3000"
+  console.log(parserLink.protocol); // If we use http or https
+  console.log(parserLink.hostname); // dev2.finastra.com only url
+  console.log(parserLink.port); // port "80" or "8080" or "3000"
+  console.log(parserLink.pathname); // after hostname/search, search is path name
+  console.log(parserLink.hash); // if we use hash #my-anchor
+  console.log(parserLink.search); // if you search something, in my case I search hello
+  console.log(parserLink.origin); // origin is full host with protocol and port, in my case https://dev2.finastra.com:port
 });
--- js/script.js
+++ js/script.js
@@ -29,20 +29,22 @@ window.addEventListener("load", function() {
     allTabs[i].addEventListener("click", clickMyTab);
   }
 
-  var urlPars = document.createElement('a');
-  urlPars.href = "https://dev2.finastra.com/search?search=hello";
 
-  console.log(
-    urlPars.host + '\n' +
-    urlPars.protocol + '\n' +
-    urlPars.hostname + '\n' +
-    urlPars.port + '\n' +
-    urlPars.pathname + '\n' +
-    urlPars.hash + '\n' +
-    urlPars.search + '\n' +
-    urlPars.origin + '\n' +
-    urlPars.hostname
-  );
+  function parseUrl(url){
+    var parserLink = document.createElement('a');
+    parserLink.href = url;
+    return parserLink;
+  }
+  parserLink = parseUrl("https://dev2.finastra.com/search?search=hello");
+
+  console.log(parserLink.host); // dev2.finastra.com with port "80" or "8080" or "3000"
+  console.log(parserLink.protocol); // If we use http or https
+  console.log(parserLink.hostname); // dev2.finastra.com only url
+  console.log(parserLink.port); // port "80" or "8080" or "3000"
+  console.log(parserLink.pathname); // after hostname/search, search is path name
+  console.log(parserLink.hash); // if we use hash #my-anchor
+  console.log(parserLink.search); // if you search something, in my case I search hello
+  console.log(parserLink.origin); // origin is full host with protocol and port, in my case https://dev2.finastra.com:port
 });
 
 //# sourceMappingURL=../script.js.map
