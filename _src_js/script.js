19f559728673a064093746632797c34befe24af8
igor <igor.bulgar@wearepropeople.com>
2018-02-21 14:04:54 +0200

    Add function for Parse url.

--- _src_js/script.js
+++ _src_js/script.js
@@ -28,4 +28,18 @@ window.addEventListener("load", function() {
   for (i = 0; i < allTabs.length; i++) {
     allTabs[i].addEventListener("click", clickMyTab);
   }
+
+  var urlPars = document.createElement('a');
+  urlPars.href = "https://dev2.finastra.com/search?search=hello";
+
+  console.log(
+    urlPars.host + '\n' + // dev2.finastra.com with port "80" or "8080" or "3000"
+    urlPars.protocol + '\n' + // If we use http or https
+    urlPars.hostname + '\n' + // dev2.finastra.com only url
+    urlPars.port + '\n' + // port "80" or "8080" or "3000"
+    urlPars.pathname + '\n' + // after hostname/search, search is path name
+    urlPars.hash + '\n' + // if we use hash #my-anchor
+    urlPars.search + '\n' + // if you search something, in my case I search hello
+    urlPars.origin + '\n' // origin is full host with protocol and port, in my case https://dev2.finastra.com:port
+  );
 });
